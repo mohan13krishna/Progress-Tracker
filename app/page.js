529990dbb493317e1b3f8893d8f619a898a7a2d2
Mohan Krishna Thalla <142295454+mohan13krishna@users.noreply.github.com>
2025-07-02 18:18:48 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,7 +1,7 @@
 'use client';
 
 import { useSession, signIn } from 'next-auth/react';
-import { useRouter } from 'next/navigation';
+import { useRouter } from 'next/navigation';   
 import { useState, useEffect } from 'react';
 import Link from 'next/link';
 
