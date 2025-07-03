f97166a8f644fc50a5134340b26fada30baa70c6
Mohan Krishna Thalla <142295454+mohan13krishna@users.noreply.github.com>
2025-07-03 09:06:12 +0530

    Update page.js

--- app/intern/dashboard/page.js
+++ app/intern/dashboard/page.js
@@ -1,7 +1,7 @@
 'use client';
 
 import { useSession } from 'next-auth/react';
-import { useRouter } from 'next/navigation';
+import { useRouter } from 'next/navigation';   
 import { useEffect, useState } from 'react';
 import { signOut } from 'next-auth/react';
 import GitLabIntegration from '../../../components/dashboard/GitLabIntegration.js';   
